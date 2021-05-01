e16bd8da8c52363e4e75303c6cb25c994c0626a5
bxaxkxa <bakabenja@gmail.com>
2021-04-30 20:46:31 -0400

    + Added Player Prototype Class   - Added Collision Component to the class   - Added Meshes Component to the class   - Added Camera Component to the class

---
+++ Source/ScreamingGopher/PlayerCharacter/CanoePawn.cpp
@@ -0,0 +1,72 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "CanoePawn.h"
+#include "Components/BoxComponent.h"
+#include "Components/StaticMeshComponent.h"
+#include "Components/SkeletalMeshComponent.h"
+#include "Camera/CameraComponent.h"
+
+// Sets default values
+ACanoePawn::ACanoePawn()
+{
+ 	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	PlayerCollision = CreateDefaultSubobject<UBoxComponent>("Box Collider");
+	PlayerCollision->SetCollisionProfileName("BlockAll");
+	SetRootComponent(PlayerCollision);
+
+	CanoeMesh = CreateDefaultSubobject<UStaticMeshComponent>("Canoe");
+	CanoeMesh->SetCollisionProfileName("NoCollision");
+	CanoeMesh->SetupAttachment(PlayerCollision);
+
+	FirstRowRacer = CreateDefaultSubobject<USkeletalMeshComponent>("FirstRowRacer");
+	FirstRowRacer->SetCollisionProfileName("NoCollision");
+	FirstRowRacer->SetupAttachment(PlayerCollision);
+
+	SecondRowRacer = CreateDefaultSubobject<USkeletalMeshComponent>("SecondRowRacer");
+	SecondRowRacer->SetCollisionProfileName("NoCollision");
+	SecondRowRacer->SetupAttachment(PlayerCollision);
+
+	ThirdRowRacer = CreateDefaultSubobject<USkeletalMeshComponent>("ThirdRowRacer");
+	ThirdRowRacer->SetCollisionProfileName("NoCollision");
+	ThirdRowRacer->SetupAttachment(PlayerCollision);
+
+	FourthRowRacer = CreateDefaultSubobject<USkeletalMeshComponent>("FourthRowRacer");
+	FourthRowRacer->SetCollisionProfileName("NoCollision");
+	FourthRowRacer->SetupAttachment(PlayerCollision);
+
+	FifthRowRacer = CreateDefaultSubobject<USkeletalMeshComponent>("FifthRowRacer");
+	FifthRowRacer->SetCollisionProfileName("NoCollision");
+	FifthRowRacer->SetupAttachment(PlayerCollision);
+
+	SixthRowRacer = CreateDefaultSubobject<USkeletalMeshComponent>("SixthRowRacer");
+	SixthRowRacer->SetCollisionProfileName("NoCollision");
+	SixthRowRacer->SetupAttachment(PlayerCollision);
+
+	FollowCamera = CreateDefaultSubobject<UCameraComponent>("FollowCamera");
+	FollowCamera->SetupAttachment(PlayerCollision);
+}
+
+// Called when the game starts or when spawned
+void ACanoePawn::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ACanoePawn::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void ACanoePawn::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+
---
+++ Source/ScreamingGopher/PlayerCharacter/CanoePawn.h
@@ -0,0 +1,63 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Pawn.h"
+#include "CanoePawn.generated.h"
+
+UCLASS()
+class SCREAMINGGOPHER_API ACanoePawn : public APawn
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this pawn's properties
+	ACanoePawn();
+
+	UPROPERTY(EditAnywhere, Category = "Collision Component")
+		class UBoxComponent* PlayerCollision;
+
+#pragma region Mesh Component
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Canoe")
+		class UStaticMeshComponent* CanoeMesh;
+
+	//Six Racer Mesh
+	UPROPERTY(EditAnywhere, Category = "Racers")
+		class USkeletalMeshComponent* FirstRowRacer;
+
+	UPROPERTY(EditAnywhere, Category = "Racers")
+		class USkeletalMeshComponent* SecondRowRacer;
+
+	UPROPERTY(EditAnywhere, Category = "Racers")
+		class USkeletalMeshComponent* ThirdRowRacer;
+
+	UPROPERTY(EditAnywhere, Category = "Racers")
+		class USkeletalMeshComponent* FourthRowRacer;
+
+	UPROPERTY(EditAnywhere, Category = "Racers")
+		class USkeletalMeshComponent* FifthRowRacer;
+
+	UPROPERTY(EditAnywhere, Category = "Racers")
+		class USkeletalMeshComponent* SixthRowRacer;
+
+#pragma endregion
+
+	UPROPERTY(EditAnywhere, Category = "Camera")
+		class UCameraComponent* FollowCamera;
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+	void RowRight(float Power);
+
+};
